a21d3986285f88e406c73686c668535b138a2020
Tiago Lopes <saxtiago14@gmail.com>
2023-05-02 22:05:45 -0300

    Feat: file index and app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { RenderScale } from "./Pages/RenderScage"
+
+
+export const App = () => {
+
+  return (
+    <RenderScale/>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './Pages/RenderScage/index.tsx';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
