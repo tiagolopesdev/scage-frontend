26af8c20b6a28e19df6777eb76b728040e71e680
Tiago Lopes <saxtiago14@gmail.com>
2023-05-06 21:48:51 -0300

    Feat: input component created

---
+++ src/Components/Input/index.tsx
@@ -0,0 +1,24 @@
+import { TextField } from "@mui/material"
+import React from "react"
+
+interface IInputProps {
+  label: string,
+  onClick?: React.MouseEventHandler<HTMLDivElement> | undefined
+}
+
+export const Input = (props: IInputProps) => {
+
+  return (
+    <TextField
+      size="medium"
+      id="standard-basic"
+      label={props.label}
+      variant="outlined"
+      style={{
+        marginRight: '15px',
+        width: '100%'
+      }}
+      onClick={props.onClick}
+    />
+  )
+}
