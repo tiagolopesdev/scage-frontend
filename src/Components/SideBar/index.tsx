a1b53f27a82ce25898eda5357bf1e8983df5a856
Tiago Lopes <saxtiago14@gmail.com>
2023-05-08 21:52:58 -0300

    Feat: called api to get users

--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -4,9 +4,29 @@ import { IconButton } from "@mui/material"
 import IconFilter from "../../Assets/filter_search.svg"
 import { Icon } from "../Img"
 import { User } from "../Users"
+import { useEffect, useState } from "react"
+import { getAllUsersService } from "../../Services/Users"
+import { IUser } from "../../@types/IUser"
 
 
 export const SideBar = () => {
+
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  const getAllUsers = async () => {
+    try {
+
+      const responseApi: IUser[] = await getAllUsersService();
+
+      setUsers(responseApi);
+
+    } catch (exception) {
+    }
+  }
+
+  useEffect(() => {
+    getAllUsers()
+  }, [])
 
   return (
     <SidebarContainer>      
