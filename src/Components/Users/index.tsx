94d454a957a4e17d916d839f2f5ddfa5af15522f
Tiago Lopes <saxtiago14@gmail.com>
2023-05-08 22:19:59 -0300

    Feat: render users to display

--- src/Components/Users/index.tsx
+++ src/Components/Users/index.tsx
@@ -3,55 +3,70 @@ import IconUser from "../../Assets/icon_user.svg"
 import IconUserEdit from "../../Assets/icon_user_edit.svg"
 import IconUserDelete from "../../Assets/icon_user_delete.svg"
 import { Card, CardActions, CardContent, IconButton, Typography } from "@mui/material"
+import { IUser } from "../../@types/IUser"
 
+interface IUsersProps {
+  users: IUser[];
+}
 
-export const User = () => {
+export const User = (usersProps: IUsersProps) => {
 
-  return (
-    <Card
-      style={{
-        borderRadius: '15px',
-        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-        backgroundColor: 'rgba(0, 0, 0, 0.1) 0px 2px 4px'
-      }}
-    >
-      <CardContent
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          padding: '0.2rem'
-        }}
-      >
-        <Icon src={String(IconUser)} style={{ margin: '0px 20px 0px 10px', width: '3rem' }} />
-        <div style={{ width: '100%' }} >
-          <Typography
-            variant="h6"
+  const { users } = usersProps;
+
+  const renderUsers = () => {
+    return (
+      users.map((user) => {
+        return (
+          <Card
             style={{
-              fontFamily: 'Dosis',
-              fontWeight: '900',
-              fontSize: 'x-large',
-              lineHeight: '1.1'
-            }}
-          >Nome usuário</Typography>
-          <Typography
-            variant="body2"
-            style={{ 
-              fontFamily: 'Dosis',
-              fontWeight: '600'
+              borderRadius: '15px',
+              boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+              backgroundColor: 'rgba(0, 0, 0, 0.1) 0px 2px 4px',
+              marginTop: '10px'
             }}
           >
-            testefalou@gmail.com
-          </Typography>
-        </div>
-        <CardActions>
-          <IconButton>
-            <Icon src={String(IconUserEdit)} />
-          </IconButton>
-          <IconButton>
-            <Icon src={String(IconUserDelete)} />
-          </IconButton>
-        </CardActions>
-      </CardContent>
-    </Card>
-  )
+            <CardContent
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '0.2rem'
+              }}
+            >
+              <Icon src={String(IconUser)} style={{ margin: '0px 20px 0px 10px', width: '3rem' }} />
+              <div style={{ width: '100%' }} >
+                <Typography
+                  variant="h6"
+                  style={{
+                    fontFamily: 'Dosis',
+                    fontWeight: '900',
+                    fontSize: 'x-large',
+                    lineHeight: '1.1'
+                  }}
+                >{user.name}</Typography>
+                <Typography
+                  variant="body2"
+                  style={{
+                    fontFamily: 'Dosis',
+                    fontWeight: '600'
+                  }}
+                >
+                  {user.email}
+                </Typography>
+              </div>
+              <CardActions>
+                <IconButton>
+                  <Icon src={String(IconUserEdit)} />
+                </IconButton>
+                <IconButton>
+                  <Icon src={String(IconUserDelete)} />
+                </IconButton>
+              </CardActions>
+            </CardContent>
+          </Card>
+        )
+      })
+    )
+  }
+
+  return <> {renderUsers()}  </>
 }
