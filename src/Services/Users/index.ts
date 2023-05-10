994a3f6aa698c585608429b793811afdd45941d8
Tiago Lopes <saxtiago14@gmail.com>
2023-05-09 22:05:00 -0300

    Feat: method to create channel to get users by filters

--- src/Services/Users/index.ts
+++ src/Services/Users/index.ts
@@ -1,5 +1,15 @@
+import { Sex } from "../../@types/Sex";
 import { userChannel } from "../Bases/api"
 
+
+export const getAllUsersByFiltersService = async (name: string, sex?: Sex) => {
+
+  const uri = sex ? `name=${name}&sex=${sex}` : `name=${name}`
+
+  return await userChannel.get(`apiUser/filters?${uri}`)
+    .then((response) => response.data.data)
+    .catch((response) => response.message);
+}
 
 export const getAllUsersService = async () => {
   return await userChannel.get("api/User")
