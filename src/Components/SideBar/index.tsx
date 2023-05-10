adb3381ceba465ddf7c733590edcb88f5f90c268
Tiago Lopes <saxtiago14@gmail.com>
2023-05-10 09:03:09 -0300

    Feat: method created to called API with base on input of the search

--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -1,17 +1,18 @@
-import { Search, SidebarContainer } from "./style"
+import { ScroolCustom, Search, SidebarContainer } from "./style"
 import { Input } from "../Input"
 import { IconButton } from "@mui/material"
 import IconFilter from "../../Assets/filter_search.svg"
 import { Icon } from "../Img"
 import { User } from "../Users"
 import { useEffect, useState } from "react"
-import { getAllUsersService } from "../../Services/Users"
+import { getAllUsersByFiltersService, getAllUsersService } from "../../Services/Users"
 import { IUser } from "../../@types/IUser"
 
 
 export const SideBar = () => {
 
   const [users, setUsers] = useState<IUser[]>([]);
+  const [nameToFilter, setNameToFilter] = useState('');
 
   const getAllUsers = async () => {
     try {
@@ -28,11 +29,28 @@ export const SideBar = () => {
     getAllUsers()
   }, [])
 
+  const getUsersByFilter = async () => {
+    try {
+
+      const responseApi: IUser[] = await getAllUsersByFiltersService(nameToFilter);
+
+      setUsers(responseApi);      
+
+    } catch (expection) {
+
+    }
+  }
+
+  useEffect(() => {
+    nameToFilter ? getUsersByFilter() : getAllUsers()
+  }, [nameToFilter])
+
   return (
     <SidebarContainer>      
       <Search>
         <Input
           label="Digite o nome do servo"
+          onChange={(event: any) => { setNameToFilter(event.target.value) }}
         />
         <IconButton>
           <Icon src={String(IconFilter)} />
