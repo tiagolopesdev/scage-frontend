ba42a5f353dae11c7095cb910047e1edd06a49ba
Tiago Lopes <saxtiago14@gmail.com>
2023-05-10 09:01:16 -0300

    Refactor: adjusted URI to called API

--- src/Services/Users/index.ts
+++ src/Services/Users/index.ts
@@ -6,7 +6,7 @@ export const getAllUsersByFiltersService = async (name: string, sex?: Sex) => {
 
   const uri = sex ? `name=${name}&sex=${sex}` : `name=${name}`
 
-  return await userChannel.get(`apiUser/filters?${uri}`)
+  return await userChannel.get(`api/User/filters?${uri}`)
     .then((response) => response.data.data)
     .catch((response) => response.message);
 }
