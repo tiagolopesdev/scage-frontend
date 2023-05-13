7fceba454e88151ce9e0e1d124a3f95d09dbe5b2
Tiago Lopes <saxtiago14@gmail.com>
2023-05-13 10:11:19 -0300

    Feat: popover component created to user edit

---
+++ src/Components/Users/Popover/index.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Popover from '@mui/material/Popover';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+
+interface IManipulationUserProps {
+  id: string | undefined,
+  open: boolean,
+  anchorEl: HTMLButtonElement | null,
+  setAnchorEl: React.Dispatch<React.SetStateAction<HTMLButtonElement | null>>
+}
+
+export const ManipulationUser = (props: IManipulationUserProps) => {
+
+  const { id, anchorEl, open, setAnchorEl } = props
+
+  // const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  // const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+
+  // const open = Boolean(anchorEl);
+  // const id = open ? 'simple-popover' : undefined;
+
+  console.log('Open ', id)
+
+  return (
+    <div>
+      {/* <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+        Open Popover
+      </Button> */}
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={() => { setAnchorEl(null) }}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'left',
+        }}
+      >
+        <div style={{ minWidth: '17rem' }}>
+          <div
+            style={{
+              margin: '0px 20px 15px 20px',
+              display: 'flex',
+              flexDirection: 'column-reverse'
+            }}
+          >
+            <TextField style={{ marginTop: '20px' }} id="standard-basic" label="Sexo" variant="standard" />
+            <TextField style={{ marginTop: '20px' }} id="standard-basic" label="E-mail" variant="standard" />
+            <TextField style={{ marginTop: '20px' }} id="standard-basic" label="Nome" variant="standard" />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              margin: '30px 20px 15px 20px',
+            }}
+          >
+            <Button
+              variant="outlined"
+              size='small'
+              color='error'
+              onClick={() => { setAnchorEl(null) }}
+              fullWidth
+            >Cancelar</Button>
+            <Button
+              style={{
+                marginLeft: '20px'
+              }}
+              variant="contained"
+              color='primary'
+              size='small'
+              onClick={() => { setAnchorEl(null) }}
+              fullWidth
+            >Salvar</Button>
+          </div>
+        </div>
+      </Popover>
+    </div>
+  );
+}
--- src/Components/Users/index.tsx
+++ src/Components/Users/index.tsx
@@ -4,6 +4,8 @@ import IconUserEdit from "../../Assets/icon_user_edit.svg"
 import IconUserDelete from "../../Assets/icon_user_delete.svg"
 import { Card, CardActions, CardContent, IconButton, Typography } from "@mui/material"
 import { IUser } from "../../@types/IUser"
+import { ManipulationUser } from "./Popover"
+import { useState } from "react"
 
 interface IUsersProps {
   users: IUser[];
@@ -13,60 +15,88 @@ export const User = (usersProps: IUsersProps) => {
 
   const { users } = usersProps;
 
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  // console.log('id ', id)
+
   const renderUsers = () => {
     return (
-      users.map((user) => {
+      users.map((user, index) => {
         return (
-          <Card
-            style={{
-              borderRadius: '15px',
-              boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-              backgroundColor: 'rgba(0, 0, 0, 0.1) 0px 2px 4px',
-              marginTop: '10px'
-            }}
-          >
-            <CardContent
+            <Card
               style={{
-                display: 'flex',
-                alignItems: 'center',
-                padding: '0.2rem'
+                borderRadius: '15px',
+                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+                backgroundColor: 'rgba(0, 0, 0, 0.1) 0px 2px 4px',
+                marginTop: '10px'
               }}
             >
-              <Icon src={String(IconUser)} style={{ margin: '0px 20px 0px 10px', width: '3rem' }} />
-              <div style={{ width: '100%' }} >
-                <Typography
-                  variant="h6"
-                  style={{
-                    fontFamily: 'Dosis',
-                    fontWeight: '900',
-                    fontSize: 'x-large',
-                    lineHeight: '1.1'
-                  }}
-                >{user.name}</Typography>
-                <Typography
-                  variant="body2"
-                  style={{
-                    fontFamily: 'Dosis',
-                    fontWeight: '600'
-                  }}
-                >
-                  {user.email}
-                </Typography>
-              </div>
-              <CardActions>
-                <IconButton>
-                  <Icon src={String(IconUserEdit)} />
-                </IconButton>
-                <IconButton>
-                  <Icon src={String(IconUserDelete)} />
-                </IconButton>
-              </CardActions>
-            </CardContent>
-          </Card>
+              <CardContent
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  padding: '0.2rem'
+                }}
+              >
+                <Icon src={String(IconUser)} style={{ margin: '0px 20px 0px 10px', width: '3rem' }} />
+                <div style={{ width: '100%' }} >
+                  <Typography
+                    variant="h6"
+                    style={{
+                      fontFamily: 'Dosis',
+                      fontWeight: '900',
+                      fontSize: 'x-large',
+                      lineHeight: '1.1'
+                    }}
+                  >{user.name}</Typography>
+                  <Typography
+                    variant="body2"
+                    style={{
+                      fontFamily: 'Dosis',
+                      fontWeight: '600'
+                    }}
+                  >
+                    {user.email}
+                  </Typography>
+                </div>
+                <CardActions>
+                  <IconButton
+                    aria-describedby={id}
+                    onClick={handleClick}
+                  >
+                    <Icon
+                      src={String(IconUserEdit)}
+                    />
+                  </IconButton>
+                  <IconButton>
+                    <Icon src={String(IconUserDelete)} />
+                  </IconButton>
+                </CardActions>
+              </CardContent>
+            </Card>
         )
       })
     )
   }
 
-  return <> {renderUsers()}  </>
+  return (
+    <>
+      {renderUsers()}
+      {open ? 
+        <ManipulationUser
+          id={id}
+          anchorEl={anchorEl}
+          open={open}
+          setAnchorEl={setAnchorEl}
+        /> : ''
+      }
+    </>
+  )
 }
