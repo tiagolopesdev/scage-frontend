502456781273c2bd5db88efb3c3cc5c91a333a60
Tiago Lopes <saxtiago14@gmail.com>
2023-05-16 20:20:37 -0300

    Refactor: created component to display only one user and adjusted popover to trigger renderition user list after manipulation

--- src/Components/Users/Popover/index.tsx
+++ src/Components/Users/Popover/index.tsx
@@ -4,22 +4,64 @@ import { FormControlLabel, Radio, RadioGroup, TextField } from '@mui/material';
 import { ButtonContainer, TextFieldContainer } from './style';
 import { IUser } from '../../../@types/IUser';
 import { Sex } from '../../../@types/Sex';
-import { useState } from 'react';
+import { Dispatch, useState } from 'react';
+import { updateUser } from '../../../Services/Users';
+import toast, { Toaster } from 'react-hot-toast';
 
 interface IManipulationUserProps {
   id: string | undefined,
   open: boolean,
   anchorEl: HTMLButtonElement | null,
   setAnchorEl: React.Dispatch<React.SetStateAction<HTMLButtonElement | null>>
-  user: IUser
+  user: IUser,
+  setUserWasManipuled: Dispatch<React.SetStateAction<boolean>>
 }
 
 export const ManipulationUser = (props: IManipulationUserProps) => {
 
-  const { id, anchorEl, open, setAnchorEl, user } = props
+  const { id, anchorEl, open, setAnchorEl, user, setUserWasManipuled } = props
 
   const [optionMasc, setOptionMasc] = useState(user.sex === Sex.MASCULINO ? true : false);
   const [optionFem, setOptionFem] = useState(user.sex === Sex.FEMININO ? true : false);
+  const [userManipulation, setUserManipulation] = useState<IUser>({
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    sex: user.sex,
+    isEnable: true
+  });
+
+  const identificationTypeSex = () => {
+    if (optionMasc) {
+      return Sex.MASCULINO
+    } else {
+      return Sex.FEMININO
+    }
+  }
+
+  const editUser = async () => {
+    try {
+
+      const userToEdit: IUser = {
+        id: userManipulation.id,
+        name: userManipulation.name,
+        email: userManipulation.email,
+        sex: identificationTypeSex(),
+        isEnable: userManipulation.isEnable,
+      }
+
+      const responseApi = await updateUser(userToEdit);
+
+      toast.success(responseApi)
+      
+      setAnchorEl(null)
+      setUserWasManipuled(true);
+      
+    } catch (error: any) {
+      console.log('Error ', error)
+      toast.error(error)
+    }
+  }
 
   const StyleButtonCustom = (styleCustom?: any) => ({
     borderRadius: '15px',
@@ -52,8 +94,8 @@ export const ManipulationUser = (props: IManipulationUserProps) => {
               control={
                 <Radio
                   checked={optionMasc}
-                  onClick={() => { 
-                    setOptionMasc(!optionMasc) 
+                  onClick={() => {
+                    setOptionMasc(!optionMasc)
                     setOptionFem(!optionFem)
                   }}
                 />
@@ -64,16 +106,30 @@ export const ManipulationUser = (props: IManipulationUserProps) => {
               control={
                 <Radio
                   checked={optionFem}
-                  onClick={() => { 
-                    setOptionFem(!optionFem) 
+                  onClick={() => {
+                    setOptionFem(!optionFem)
                     setOptionMasc(!optionMasc)
                   }}
                 />
               }
             />
           </RadioGroup>
-          <TextField defaultValue={user.email} style={{ marginTop: '20px' }} id="standard-basic" label="E-mail" variant="standard" />
-          <TextField defaultValue={user.name} style={{ marginTop: '20px' }} id="standard-basic" label="Nome" variant="standard" />
+          <TextField
+            label="E-mail"
+            variant="standard"
+            id="standard-basic"
+            defaultValue={user.email}
+            style={{ marginTop: '20px' }}
+            onChange={(event: any) => { setUserManipulation({ ...userManipulation, email: event.target.value }) }}
+          />
+          <TextField
+            label="Nome"
+            variant="standard"
+            id="standard-basic"
+            defaultValue={user.name}
+            style={{ marginTop: '20px' }}
+            onChange={(event: any) => { setUserManipulation({ ...userManipulation, name: event.target.value }) }}
+          />
         </TextFieldContainer>
         <ButtonContainer>
           <Button
@@ -91,11 +147,15 @@ export const ManipulationUser = (props: IManipulationUserProps) => {
             })}
             variant="contained"
             size='small'
-            onClick={() => { setAnchorEl(null) }}
+            onClick={() => { editUser() }}
             fullWidth
           >Salvar</Button>
         </ButtonContainer>
       </div>
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+      />
     </Popover>
   );
 }
