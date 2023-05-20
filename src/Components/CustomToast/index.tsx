40aa15f20289620960636713f315cf9b14587555
Tiago Lopes <saxtiago14@gmail.com>
2023-05-20 20:04:43 -0300

    Refactor: changed component to reuse

--- src/Components/CustomToast/index.tsx
+++ src/Components/CustomToast/index.tsx
@@ -1,8 +1,18 @@
 import { toast } from "react-hot-toast"
 import iconSuccess from "../../Assets/icon_success.svg"
+import iconError from "../../Assets/icon_error.svg"
 
+interface ICustomToast {
+  message: string,
+  duration: number,
+  isError: boolean
+}
 
-export const CustomToast = () => {
+export const CustomToast = (props: ICustomToast) => {
+
+  const { duration, isError, message } = props
+
+  const typeIconDisplay = isError ? String(iconError) : String(iconSuccess);
 
   const displayToast = () => {
     return toast.custom(
@@ -13,19 +23,21 @@ export const CustomToast = () => {
         display: 'flex',
         alignItems: "center"
       }}>
-        <img src={String(iconSuccess)} style={{ width: '25px', marginLeft: '15px' }} />
+        <img src={typeIconDisplay}
+          style={{ width: '25px', marginLeft: '15px' }}
+        />
         <p style={{
           fontFamily: 'Dosis',
           fontSize: '1rem',
           fontWeight: '600',
           margin: '10px 15px 10px 5px'
-        }} >Usuário criado com sucesso</p>
+        }} >{message}</p>
       </div>,
       {
-        duration: 2000
+        duration: duration
       }
     )
   }
 
-  return <> { displayToast() } </>
+  return <> {displayToast()} </>
 }
