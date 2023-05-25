ac1427817774e96b667c3ece50a4d009f2fa5583
Tiago Lopes <saxtiago14@gmail.com>
2023-05-25 20:24:38 -0300

    Refactor: applying styled components createds

--- src/Components/CustomToast/index.tsx
+++ src/Components/CustomToast/index.tsx
@@ -1,6 +1,7 @@
 import { toast } from "react-hot-toast"
 import iconSuccess from "../../Assets/icon_success.svg"
 import iconError from "../../Assets/icon_error.svg"
+import { TextStyle, ToastContainer } from "./style"
 
 interface ICustomToast {
   message: string,
@@ -16,26 +17,13 @@ export const CustomToast = (props: ICustomToast) => {
 
   const displayToast = () => {
     return toast.custom(
-      <div style={{
-        backgroundColor: '#FFFFFF',
-        boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.25)',
-        borderRadius: '15px',
-        display: 'flex',
-        alignItems: "center"
-      }}>
+      <ToastContainer>
         <img src={typeIconDisplay}
           style={{ width: '25px', marginLeft: '15px' }}
         />
-        <p style={{
-          fontFamily: 'Dosis',
-          fontSize: '1rem',
-          fontWeight: '600',
-          margin: '10px 15px 10px 5px'
-        }} >{message}</p>
-      </div>,
-      {
-        duration: duration
-      }
+        <TextStyle>{message}</TextStyle>
+      </ToastContainer>,
+      { duration: duration }
     )
   }
 
