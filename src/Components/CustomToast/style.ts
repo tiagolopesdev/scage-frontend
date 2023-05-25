45fb0300114e49d9b2b7446816320a1366806aab
Tiago Lopes <saxtiago14@gmail.com>
2023-05-25 20:19:12 -0300

    Style: styled components created to use at custom toast

---
+++ src/Components/CustomToast/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const TextStyle = styled.p`
+  font-family: Dosis;
+  font-size: 1rem;
+  font-weight: 600;
+  margin: 10px 15px 10px 5px;
+`
+
+export const ToastContainer = styled.div`
+  background-color: #FFFFFF;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+`
