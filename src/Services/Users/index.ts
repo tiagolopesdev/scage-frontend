8376477899316b8e9751e79929b28ebed70b7a85
Tiago Lopes <saxtiago14@gmail.com>
2023-05-25 21:17:35 -0300

    Refactor: changed return at error case

--- src/Services/Users/index.ts
+++ src/Services/Users/index.ts
@@ -32,5 +32,5 @@ export const getAllUsersByFiltersService = async (name?: string, sex?: string) =
 export const getAllUsersService = async () => {
   return await userChannel.get("api/User")
     .then((response) => response.data.data)
-    .catch((response) => response.message);
+    .catch((response) => { return response });
 }
