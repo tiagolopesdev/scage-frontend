fc0d7d60fb21710b208c7a1518b49b7fa85e965c
Tiago Lopes <saxtiago14@gmail.com>
2023-05-27 12:15:52 -0300

    Refactor: removed div

--- src/Pages/RenderScage/index.tsx
+++ src/Pages/RenderScage/index.tsx
@@ -3,14 +3,9 @@ import { UserListFloating } from '../../Components/Users/user-list-floating';
 
 export const RenderScale = () =>  {
   return ( 
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end'
-      }}
-    >
+    <>
       <NavBar/>
       <UserListFloating/>
-    </div>
+    </>
   );
 }
