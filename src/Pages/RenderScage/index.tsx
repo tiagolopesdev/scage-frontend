b10af6760dc608bd981af263f0531ad9ea919524
Tiago Lopes <saxtiago14@gmail.com>
2023-06-03 13:01:09 -0300

    Feat: displaying card at page

--- src/Pages/RenderScage/index.tsx
+++ src/Pages/RenderScage/index.tsx
@@ -1,11 +1,41 @@
+import { CardDay } from '../../Components/Cards/Day';
 import { NavBar } from '../../Components/Navbar';
 import { UserListFloating } from '../../Components/Users/user-list-floating';
 
-export const RenderScale = () =>  {
-  return ( 
+export const RenderScale = () => {
+  return (
     <>
-      <NavBar/>
-      <UserListFloating/>
+      <NavBar />
+      <UserListFloating />
+      <div
+        style={{
+          margin: '1%',
+          position: 'fixed',
+          left: '0'
+        }}
+      >
+        <div
+          style={{
+            margin: '0% 25rem 2% 2%',
+            display: 'flex',
+            flexWrap: 'wrap',
+            scrollBehavior: 'smooth',
+            overflowY: 'scroll',
+            right: '40%',
+            height: '37rem'
+          }}
+        >
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+        </div>
+      </div>
     </>
   );
 }
