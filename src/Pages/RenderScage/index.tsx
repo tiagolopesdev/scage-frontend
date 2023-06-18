e7c8cdd5e44d61d0ab45a3899b70407320ee2bfb
Tiago Lopes <saxtiago14@gmail.com>
2023-06-18 14:48:29 -0300

    Feat: new group buttons to actions

--- src/Pages/RenderScage/index.tsx
+++ src/Pages/RenderScage/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import { CardDay } from '../../Components/Cards/Day/index';
 import { NavBar } from '../../Components/Navbar';
 import { UserListFloating } from '../../Components/Users/user-list-floating';
@@ -8,23 +9,85 @@ export const RenderScale = () => {
   return (
     <>
       <NavBar />
-      <CardDayContainer>
-        <ScroolCustom
+      <div>
+        <CardDayContainer>
+          <ScroolCustom
+            style={{
+              margin: '0% 25rem 2% 2%',
+              display: 'flex',
+              flexWrap: 'wrap',
+              right: '40%'
+            }}
+          >
+            <CardDay />
+            <CardDay />
+            <CardDay />
+            <CardDay />
+            <CardDay />
+            <CardDay />
+          </ScroolCustom>
+        </CardDayContainer>
+        <div
           style={{
-            margin: '0% 25rem 2% 2%',
-            display: 'flex',
-            flexWrap: 'wrap',
-            right: '40%'
+            position: 'absolute',
+            bottom: '0px',
+            backgroundColor: '#CBCBCB',
+            width: '100%',
+            boxShadow: '0px -6px 15px rgba(0, 0, 0, 0.3)'
           }}
         >
-          <CardDay />
-          <CardDay />
-          <CardDay />
-          <CardDay />
-          <CardDay />
-          <CardDay />
-        </ScroolCustom>
-      </CardDayContainer>
+          <div
+            style={{
+              padding: '20px',
+              display: 'flex',
+              maxWidth: '60%'
+            }}
+          >
+            <Button
+              style={{
+                borderRadius: '12px',
+                fontFamily: 'Dosis',
+                textTransform: 'none',
+                fontSize: '1rem',
+                fontWeight: '600',
+                backgroundColor: 'rgb(14, 202, 101)',
+                margin: '0% 2% 0% 3%'
+              }}
+              variant="contained"
+              size='small'
+            fullWidth
+            >Gerar nova escala</Button>
+            <Button
+              style={{
+                borderRadius: '12px',
+                fontFamily: 'Dosis',
+                textTransform: 'none',
+                fontSize: '1rem',
+                fontWeight: '600',
+                backgroundColor: 'rgb(14, 202, 101)',
+                margin: '0% 3% 0% 3%'
+              }}
+              variant="contained"
+              size='small'
+            fullWidth
+            >Adicionar novo dia</Button>
+            <Button
+              style={{
+                borderRadius: '12px',
+                fontFamily: 'Dosis',
+                textTransform: 'none',
+                fontSize: '1rem',
+                fontWeight: '600',
+                backgroundColor: '#30B2DB',
+                margin: '0% 3% 0% 3%'
+              }}
+              variant="contained"
+              size='small'
+            fullWidth
+            >Exportar escala em PDF</Button>
+          </div>
+        </div>
+      </div>
       <UserListFloating />
     </>
   );
