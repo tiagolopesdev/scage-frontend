0690770f8543f93d9f64913493c3847d128ba90b
Tiago Lopes <saxtiago14@gmail.com>
2023-06-19 21:23:14 -0300

    Style: styled component to button group

--- src/Pages/RenderScage/style.ts
+++ src/Pages/RenderScage/style.ts
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+export const ButtonGroupContainer = styled.div`
+  position: absolute;
+  bottom: 0px;
+  background-color: #CBCBCB;
+  width: 100%;
+  box-shadow: 0px -6px 15px rgba(0, 0, 0, 0.3);
+`;
 
 export const CardDayContainer = styled.div`
   margin: 1%;
