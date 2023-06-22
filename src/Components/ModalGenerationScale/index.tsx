b83f56430420402313743b8dd2f989f7f9cf30de
Tiago Lopes <saxtiago14@gmail.com>
2023-06-21 21:24:08 -0300

    Feat: adjusted code to displaying modal day

--- src/Components/ModalGenerationScale/index.tsx
+++ src/Components/ModalGenerationScale/index.tsx
@@ -15,6 +15,8 @@ import { Icon } from "../Img";
 import IconEdit from '../../Assets/icon_user_edit.svg'
 import IconDelete from '../../Assets/icon_trash.svg'
 import { ButtonGroup, ContainerNewDay } from "./style";
+import { ModalDay } from "../ModalDay";
+import { useState } from "react";
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -36,6 +38,8 @@ export const ModalGenerationScale = (props: IModalGenerationScale) => {
 
   const { openModal, openModalState } = props
 
+  const [openModalNewDay, setOpenModalNewDay] = useState(false);
+
   const HandlerClose = () => {
     openModalState(!openModal)
   }
@@ -50,64 +54,74 @@ export const ModalGenerationScale = (props: IModalGenerationScale) => {
   })
 
   return (
-    <Modal
-      open={openModal}
-      onClose={() => { HandlerClose() }}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-        <ContainerNewDay>
-          <Button
-            style={ButtonStyleCustom({ backgroundColor: 'rgb(14, 202, 101)', marginBottom: '4%' })}
-            variant="contained"
-            size='small'
-          >Adicionar novo dia</Button>
-        </ContainerNewDay>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell style={{ fontFamily: 'Dosis', fontSize: 'larger' }} >{<strong>Nome</strong>}</TableCell>
-                <TableCell style={{ fontFamily: 'Dosis', fontSize: 'larger' }} align="center">{<strong>Data</strong>}</TableCell>
-                <TableCell style={{ fontFamily: 'Dosis', fontSize: 'larger' }} align="center">{<strong>Horário</strong>}</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
-                <TableCell component="th" scope="row">
-                  Missa dominical dsdsdsds dsdsdsd
-                </TableCell>
-                <TableCell align="center">00/00/000</TableCell>
-                <TableCell align="center">00:00</TableCell>
-                <TableCell align="right" style={{ padding: '0rem 0.5rem 0rem 0.5rem' }}>
-                  <IconButton>
-                    <Icon src={String(IconEdit)} />
-                  </IconButton>
-                  <IconButton>
-                    <Icon src={String(IconDelete)} />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <ButtonGroup>
-          <Button
-            style={ButtonStyleCustom({ color: '#CA0E0E', border: '1px solid #CA0E0E' })}
-            variant="outlined"
-            size='small'
-            fullWidth
-            onClick={() => { HandlerClose() }}
-          >Cancelar</Button>
-          <Button
-            style={ButtonStyleCustom({ backgroundColor: 'rgb(14, 202, 101)', marginLeft: '30px' })}
-            variant="contained"
-            size='small'
-            fullWidth
-          >Gerar escala</Button>
-        </ButtonGroup>
-      </Box>
-    </Modal >
+    <>
+      <Modal
+        open={openModal}
+        onClose={() => { HandlerClose() }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <ContainerNewDay>
+            <Button
+              size='small'
+              variant="contained"
+              onClick={() => { setOpenModalNewDay(!openModalNewDay) }}
+              style={ButtonStyleCustom({ backgroundColor: 'rgb(14, 202, 101)', marginBottom: '4%' })}
+            >Adicionar novo dia</Button>
+          </ContainerNewDay>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ fontFamily: 'Dosis', fontSize: 'larger' }} >{<strong>Nome</strong>}</TableCell>
+                  <TableCell style={{ fontFamily: 'Dosis', fontSize: 'larger' }} align="center">{<strong>Data</strong>}</TableCell>
+                  <TableCell style={{ fontFamily: 'Dosis', fontSize: 'larger' }} align="center">{<strong>Horário</strong>}</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+                  <TableCell component="th" scope="row">
+                    Missa dominical dsdsdsds dsdsdsd
+                  </TableCell>
+                  <TableCell align="center">00/00/000</TableCell>
+                  <TableCell align="center">00:00</TableCell>
+                  <TableCell align="right" style={{ padding: '0rem 0.5rem 0rem 0.5rem' }}>
+                    <IconButton>
+                      <Icon src={String(IconEdit)} />
+                    </IconButton>
+                    <IconButton>
+                      <Icon src={String(IconDelete)} />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <ButtonGroup>
+            <Button
+              style={ButtonStyleCustom({ color: '#CA0E0E', border: '1px solid #CA0E0E' })}
+              variant="outlined"
+              size='small'
+              fullWidth
+              onClick={() => { HandlerClose() }}
+            >Cancelar</Button>
+            <Button
+              style={ButtonStyleCustom({ backgroundColor: 'rgb(14, 202, 101)', marginLeft: '30px' })}
+              variant="contained"
+              size='small'
+              fullWidth
+            >Gerar escala</Button>
+          </ButtonGroup>
+        </Box>
+      </Modal >
+      {
+        openModalNewDay ?
+          <ModalDay
+            openModal={openModalNewDay}
+            openModalState={setOpenModalNewDay}
+          /> : ''
+      }
+    </>
   )
 }
