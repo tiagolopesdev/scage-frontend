db506c4c7b5d16f3b24640b7985bb2e8b74b9648
Tiago Lopes <saxtiago14@gmail.com>
2023-06-23 22:43:21 -0300

    Feat: settings to use date and time of the Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react"
+import { LocalizationProvider } from "@mui/x-date-pickers"
 import { RenderScale } from "./Pages/RenderScage"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 
 
 export const App = () => {
 
   return (
-    <RenderScale/>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <RenderScale />
+    </LocalizationProvider>
   )
 }
