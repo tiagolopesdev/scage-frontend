99c73b4e5a0aa1b7292be6da95dc48f061f465e0
Tiago Lopes <saxtiago14@gmail.com>
2023-07-18 21:55:32 -0300

    Feat: const with months

---
+++ src/@types/Months.ts
@@ -0,0 +1,16 @@
+
+
+export enum Months {
+  JANUARY = 'Janeiro',
+  FEBRUARY = 'Fevereiro',
+  MARCH = 'Março',
+  APRIL = 'Abril',
+  MAY = 'Março',
+  JUNE = 'Junho',
+  JULY = 'Julho',
+  AUGUST = 'Agosto',
+  SEPTEMBER = 'Setembro',
+  OCTOBER = 'Outubro',
+  NOVEMBER = 'Novembro',
+  DECEMBER = 'Dezembro'
+}
