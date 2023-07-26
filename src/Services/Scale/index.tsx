aa1b2b0fbdfa4eba9425d52172f12ecf2ae986ad
Tiago Lopes <saxtiago14@gmail.com>
2023-07-26 08:54:48 -0300

    Feat: channel to save scale

--- src/Services/Scale/index.tsx
+++ src/Services/Scale/index.tsx
@@ -1,8 +1,15 @@
+import { IScaleMonth } from "../../@types/IScaleMonth"
 import { scaleChannel } from "../Bases/api"
 
 
+export const SaveScaleService = (scale: IScaleMonth) => {
+  return scaleChannel.post('api/Scale/createScale', scale)
+    .then((response) => response.data.data)
+    .catch((error) => error)
+}
+
 export const GenerationPreviewScale = (scale: any) => {
   return scaleChannel.post('api/Scale/generationScale', scale)
-  .then((response) => response.data.data)
-  .catch((error) => error)
+    .then((response) => response.data.data)
+    .catch((error) => error)
 }
