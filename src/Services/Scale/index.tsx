60bc81e5de426e733778ac07188c21806cf28280
Tiago Lopes <saxtiago14@gmail.com>
2023-08-09 21:53:08 -0300

    Feat: communication channel with API to get scale complete

--- src/Services/Scale/index.tsx
+++ src/Services/Scale/index.tsx
@@ -2,6 +2,12 @@ import { IScaleMonth } from "../../@types/IScaleMonth"
 import { scaleChannel } from "../Bases/api"
 
 
+export const GetScale = (scaleId: string) => {
+  return scaleChannel.get(`api/Scale/${scaleId}`)
+    .then((response) => response.data.data)
+    .catch((error) => error)  
+}
+
 export const GetSingleScales = (filter?: string) => {
   return scaleChannel.get('api/Scale/singleScales', { params: { filter } })
     .then((response) => response.data.data)
