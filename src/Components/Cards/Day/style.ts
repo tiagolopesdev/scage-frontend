35023de4837fdc065ac1ea252aac4ec86760caac
Tiago Lopes <saxtiago14@gmail.com>
2023-08-14 12:16:31 -0300

    Style: created style component

--- src/Components/Cards/Day/style.ts
+++ src/Components/Cards/Day/style.ts
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 
 
+export const NameDayStyle = styled.h5`
+  font-family: Dosis;
+  font-size: 16pt;
+  font-weight: bold;
+  margin: 10px;
+  color: #005AAC;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`
+
 export const PeopleContainer = styled.div`
   display: flex;
   margin: 5px 0px;
