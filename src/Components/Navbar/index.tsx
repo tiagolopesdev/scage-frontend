e7da606c27382245cda28af1b54605b3bba6dd33
Tiago Lopes <saxtiago14@gmail.com>
2023-08-14 12:21:33 -0300

    Feat: displaying name month and using styled component created

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,11 +1,29 @@
-import { Container, Title } from "./style"
+import { useContext } from "react";
+import { Container, ReferencyMonthNameStyle } from "./style"
+import { ScaleContext } from "../../Context/scale";
 
 
 export const NavBar = () => {
 
+  const { scale } = useContext(ScaleContext);
+
   return (
     <Container>
-      <Title>Pascom</Title>
+      {
+        scale?.name ?
+          <ReferencyMonthNameStyle>
+            <p style={{ color: '#FFFFFF', fontFamily: 'Dosis' }}>Escala referente à </p>
+            <p style={{
+              color: '#FFFFFF',
+              marginLeft: '6px',
+              fontWeight: 'bold',
+              fontFamily: 'Dosis'
+            }}>{scale?.name}</p>
+          </ReferencyMonthNameStyle> : ""
+      }
+      {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Title>Pascom</Title>
+      </div> */}
     </Container>
   )
 }
