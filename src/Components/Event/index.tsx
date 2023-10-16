7cd571fedb7f360cfce35958830a227e4c1d48dc
Tiago Lopes <saxtiago14@gmail.com>
2023-10-16 20:59:10 -0300

    Feat: displaying informations about event at the component created

--- src/Components/Event/index.tsx
+++ src/Components/Event/index.tsx
@@ -1,11 +1,12 @@
 import { Icon } from "../Img"
 import IconCalendar from "../../Assets/icon_calendar_dark.svg"
 import IconClock from "../../Assets/icon_clock_dark.svg"
+import IconChange from "../../Assets/icon_changed.svg"
 import { InformationContainerStyle, InformationGroupStyle, InformationStyle, TextStyle } from "./style"
-import { motion } from "framer-motion"
-import { Checkbox } from "@mui/material"
-import { useState } from "react"
+import { IconButton } from "@mui/material"
+import React, { useState } from "react"
 import dayjs from "dayjs"
+import { FilterDUser } from "./Popover"
 
 
 interface IEventSerf {
@@ -13,35 +14,48 @@ interface IEventSerf {
   dateTime: string
 }
 
-export const EventSerf = ({name, dateTime}: IEventSerf) => {
-
-  const [isChecked, setIsChecked] = useState(false);
+export const EventSerf = ({ name, dateTime}: IEventSerf) => {
 
   const date = dayjs(dateTime).format('DD/MM/YYYY')
   const hour = dayjs(dateTime).format('HH:mm')
 
-  return <motion.div
-    className="box"
-    whileHover={{ scale: 1.01 }}
-    whileTap={{ scale: 1.0 }}
-    transition={{ type: "spring", stiffness: 400, damping: 17 }}
-    onClick={() => { setIsChecked(!isChecked) }}
-  >
-    <div style={{ display: 'flex' }}>
-      <InformationContainerStyle>
-        <TextStyle fontSize={15} isBold={true} >{name}</TextStyle>
-        <InformationGroupStyle>
-          <InformationStyle>
-            <Icon src={String(IconCalendar)} />
-            <TextStyle fontSize={12}>{date}</TextStyle>
-          </InformationStyle>
-          <InformationStyle>
-            <Icon src={String(IconClock)} />
-            <TextStyle fontSize={12}>{hour}</TextStyle>
-          </InformationStyle>
-        </InformationGroupStyle>
-      </InformationContainerStyle>
-      <Checkbox checked={isChecked} />
-    </div>
-  </motion.div>
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover-day' : undefined;
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  return <div style={{ display: 'flex' }}>
+    <InformationContainerStyle>
+      <TextStyle fontSize={15} isBold={true} >{name}</TextStyle>
+      <InformationGroupStyle>
+        <InformationStyle>
+          <Icon src={String(IconCalendar)} />
+          <TextStyle fontSize={12}>{date}</TextStyle>
+        </InformationStyle>
+        <InformationStyle>
+          <Icon src={String(IconClock)} />
+          <TextStyle fontSize={12}>{hour}</TextStyle>
+        </InformationStyle>
+      </InformationGroupStyle>
+    </InformationContainerStyle>
+    <IconButton aria-describedby={id}
+      onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+        handleClick(event)
+      }}
+    >
+      <Icon src={String(IconChange)} />
+    </IconButton>
+    {
+      <FilterDUser
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+      />
+    }
+  </div>
 }
