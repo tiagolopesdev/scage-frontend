69947dc153d9495fe199daf817c0bce00a653939
Tiago Lopes <saxtiago14@gmail.com>
2023-10-17 22:06:16 -0300

    Feat: new properties added

--- src/Context/scale.tsx
+++ src/Context/scale.tsx
@@ -1,7 +1,19 @@
 import { ReactNode, createContext, useEffect, useState } from "react"
 import { GetScale } from "../Services/Scale";
-import { IScaleMonth } from "../@types/IScaleMonth";
+import { IDay, IScaleMonth } from "../@types/IScaleMonth";
+import { initialStateDay, initialStateUser } from "../@types/InitialStateDay";
+import { IUser } from "../@types/IUser";
 
+
+interface IFromDay {
+  day: IDay,
+  serf: IUser
+}
+
+const InitialState: IFromDay = {
+  day: initialStateDay,
+  serf: initialStateUser
+}
 
 interface IScaleContextProvider {
   children: ReactNode
@@ -14,12 +26,18 @@ interface IScaleContext {
   setScaleContext: React.Dispatch<React.SetStateAction<IScaleMonth>>
   displayScale: boolean
   setDisplayScale: React.Dispatch<React.SetStateAction<boolean>>
+  toDay: IFromDay,
+  setToDay: React.Dispatch<React.SetStateAction<IFromDay>>,
+  fromDay: IFromDay,
+  setFromDay: React.Dispatch<React.SetStateAction<IFromDay>>
 }
 
 export const ScaleContext = createContext({} as IScaleContext)
 
 export const ScaleProvider = ({ children }: IScaleContextProvider) => {
 
+  const [fromDay, setFromDay] = useState<IFromDay>(InitialState);
+  const [toDay, setToDay] = useState<IFromDay>(InitialState);
   const [scaleId, setScaleId] = useState('');
   const [scaleContext, setScaleContext] = useState<IScaleMonth>({
     name: '',
@@ -43,7 +61,18 @@ export const ScaleProvider = ({ children }: IScaleContextProvider) => {
   }, [scaleId])
 
   return (
-    <ScaleContext.Provider value={{ scaleId, setScaleId, scaleContext, setScaleContext, displayScale, setDisplayScale }} >
+    <ScaleContext.Provider value={{
+      scaleId, 
+      setScaleId, 
+      scaleContext, 
+      setScaleContext, 
+      displayScale, 
+      setDisplayScale,
+      fromDay,
+      setFromDay,
+      toDay,
+      setToDay
+    }} >
       {children}
     </ScaleContext.Provider>
   )
