a3071cd491b3b7216e56fd22fc2779fc4ad6b552
tiagolopesdev <saxtiago14@gmail.com>
2023-12-12 17:36:57 -0300

    Feat: actions button component created

---
+++ src/Components/ActionButtons/index.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@mui/material"
+import { GroupButtonStyle } from "./style"
+
+
+interface IActionButtons {
+  nameLeft: string,
+  actionLeft: () => void
+  nameRight: string,
+  actionRight: () => void
+}
+
+export const ActionButtons = (props: IActionButtons) => {
+
+  const styleButton = (margin: any) => ({
+    textTransform: 'none',
+    fontSize: '1rem',
+    fontWeight: '600',
+    borderRadius: '10px',
+    ...margin
+  })
+
+  return <GroupButtonStyle>
+    <Button
+      sx={styleButton({ marginRight: '10px' })}
+      variant="outlined"
+      size='small'
+      color='error'
+      fullWidth
+      onClick={props.actionLeft}
+    >{props.nameLeft}</Button>
+    <Button
+      sx={styleButton({ marginLeft: '10px' })}
+      onClick={props.actionRight}
+      variant="contained"
+      color="success"
+      size='small'
+      fullWidth
+    >
+      {props.nameRight}
+    </Button>
+  </GroupButtonStyle>
+}
---
+++ src/Components/ActionButtons/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const GroupButtonStyle = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 50px 0px 0px 0px;
+`
