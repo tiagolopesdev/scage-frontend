30b9eb7298b2d2fa35ed5534f2d35eed67ea176d
tiagolopesdev <saxtiago14@gmail.com>
2023-12-12 16:52:19 -0300

    Feat: object to combobox created

---
+++ src/@types/DayOfWeek.ts
@@ -0,0 +1,9 @@
+export const DayOfWeek = [
+  { label: 'Segunda-feira' },
+  { label: 'Terça-feira' },
+  { label: 'Quarta-feira' },
+  { label: 'Quinta-feira' },
+  { label: 'Sexta-feira' },
+  { label: 'Sábado' },
+  { label: 'Domingo' }
+]
