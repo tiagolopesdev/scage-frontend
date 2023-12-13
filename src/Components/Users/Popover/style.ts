b9ef0f3a001e143bf7cb6d00fa67903dd6cf6fa9
Tiago Lopes <saxtiago14@gmail.com>
2023-12-13 18:15:13 -0300

    Fix: adjusted component style

--- src/Components/Users/Popover/style.ts
+++ src/Components/Users/Popover/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const TextFieldContainer = styled.div`
   margin: 0px 30px;
   display: flex;
-  flex-direction: column-reverse
+  flex-direction: column-reverse;
 `;
 
 export const ButtonContainer = styled.div`
