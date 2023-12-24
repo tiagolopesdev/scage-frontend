d51c7d3d5594e3078a6e1298fcafd0697846ab41
Tiago Lopes <saxtiago14@gmail.com>
2023-12-24 14:10:27 -0300

    Refactor: handler to get users created

---
+++ src/Handlers/users.ts
@@ -0,0 +1,29 @@
+import { IUser } from "../@types/IUser";
+import { CustomToast } from "../Components/CustomToast";
+import { getAllUsersByFiltersService, getAllUsersService } from "../Services/Users";
+
+import IconError from '../Assets/icon_error.svg'
+
+
+interface IManagerFindUsers {
+  sex: string,
+  name: string,
+}
+
+export const managementFindUsers = async (props: IManagerFindUsers) => {
+  try {
+
+    const responseApi: IUser[] = props.name === '' && props.sex === '' ?
+      await getAllUsersService() :
+      await getAllUsersByFiltersService(props.name, props.sex)
+
+    return responseApi;
+
+  } catch (exception) {
+    CustomToast({
+      duration: 2000,
+      icon: String(IconError),
+      message: 'Não foi possível obter usuários'
+    })
+  }
+}
