606f08606ac75d7bbcdc491a52f2223f5f51e63c
Tiago Lopes <saxtiago14@gmail.com>
2023-12-24 14:31:20 -0300

    Style: components style created

---
+++ src/Components/Statistics/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+export const ContainerFiltersStyle = styled.div`
+  max-width: 25vw;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+  margin-bottom: 25px;
+`
+
+export const BadgeSizeFixedStyle = styled.div`
+  height: 1rem;
+`
+
+export const SearchStyle = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 100%;
+  width: 100%;
+`;
