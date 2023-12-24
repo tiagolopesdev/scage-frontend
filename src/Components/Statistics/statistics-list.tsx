c6dab316a52cfb58e2231d2600c039f38f442552
Tiago Lopes <saxtiago14@gmail.com>
2023-12-24 14:31:58 -0300

    Feat: implementation of the filters by name and sex

--- src/Components/Statistics/statistics-list.tsx
+++ src/Components/Statistics/statistics-list.tsx
@@ -4,6 +4,12 @@ import { managementFindUsers } from "../../Handlers/users";
 import { initialStateUser } from "../../@types/InitialStateDay";
 import { IUser } from "../../@types/IUser";
 import { ScroolCustom } from "../../Styles";
+import { Chip, IconButton } from "@mui/material";
+import { Input } from "../Input";
+import { BadgeSizeFixedStyle, ContainerFiltersStyle, SearchStyle } from "./style";
+import { FilterUser } from "../Users/Popover/filter-user";
+import IconFilter from "../../Assets/filter_search.svg"
+import { Icon } from "../Img";
 
 
 export const StatisticsList = () => {
@@ -12,16 +18,57 @@ export const StatisticsList = () => {
   const [sexFilter, setSexFilter] = useState('')
   const [users, setUsers] = useState([initialStateUser])
 
+  const [anchorFilterPopover, setAnchorFilterPopover] = useState<HTMLButtonElement | null>(null);
+  const openFilterPopover = Boolean(anchorFilterPopover);
+  const idFilter = openFilterPopover ? 'simple-popover-filter' : undefined;
+
   const usersFounded = async () => {
     const result = await managementFindUsers({ name: nameFilter, sex: sexFilter })
     setUsers(result as IUser[])
   }
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorFilterPopover(event.currentTarget);
+  };
+
   useEffect(() => {
     usersFounded()
-  }, [])
+  }, [nameFilter, sexFilter])
 
-  return <ScroolCustom style={{ width: '25vw', height: '75vh' }}>
-    <Serving users={users} isStatistics={true} />
-  </ScroolCustom>
+  return <>
+    <ContainerFiltersStyle >
+      <SearchStyle>
+        <Input
+          label="Digite o nome do colaborador"
+          onChange={(event: any) => { setNameFilter(event.target.value) }}
+        />
+        <IconButton onClick={(event: any) => { handleClick(event) }} >
+          <Icon src={String(IconFilter)} />
+        </IconButton>
+      </SearchStyle>
+      <BadgeSizeFixedStyle>
+        {sexFilter ?
+          <Chip
+            size="small"
+            label={sexFilter}
+            sx={{ marginTop: '8px', fontWeight: 600 }}
+            onDelete={() => { setSexFilter('') }} /> :
+          ''
+        }
+      </BadgeSizeFixedStyle>
+    </ContainerFiltersStyle>
+    <ScroolCustom style={{ width: '25vw', height: '60vh' }}>
+      <Serving users={users} isStatistics={true} />
+    </ScroolCustom>
+    {
+      openFilterPopover ?
+        <FilterUser
+          id={idFilter}
+          anchorEl={anchorFilterPopover}
+          open={openFilterPopover}
+          setAnchorEl={setAnchorFilterPopover}
+          setSexSelected={setSexFilter}
+        /> : ''
+    }
+  </>
 }
